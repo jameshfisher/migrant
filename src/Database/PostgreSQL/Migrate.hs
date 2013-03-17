68dff80dd9079654b3f3d22787eae1b4cd5183b4
James Fisher <jameshfisher@gmail.com>
2013-03-17 22:41:10 +0000

    fix cabal install errors

--- src/Database/PostgreSQL/Migrate.hs
+++ src/Database/PostgreSQL/Migrate.hs
@@ -2,4 +2,3 @@ module Database.PostgreSQL.Migrate (module X) where
 
 import Database.PostgreSQL.Migrate.Data as X (Migration(..))
 import Database.PostgreSQL.Migrate.Runner as X (runMigrations)
-import Database.PostgreSQL.Migrate.Shortcuts.PostgreSQL as X (addColumn)
