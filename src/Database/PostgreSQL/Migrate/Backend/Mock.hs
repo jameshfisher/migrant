69a7348416f2f6930394d3824934cbc7a7379d4a
James Fisher <jameshfisher@gmail.com>
2013-03-19 01:17:59 +0000

    test suite

---
+++ src/Database/PostgreSQL/Migrate/Backend/Mock.hs
@@ -0,0 +1,71 @@
+module Database.PostgreSQL.Migrate.Backend.Mock where
+
+import Data.Maybe
+import Data.IORef
+
+import Database.PostgreSQL.Migrate.Data
+
+type MockState = Int
+
+type MockQuery = Maybe Int -- semantics: Nothing is invalid query; (Just i) adds i to state
+
+type MockStack = Maybe [Migration MockQuery] -- head is latest
+
+data MockConnection = MockConnection {
+  mockConnectionStack :: MockStack,
+  mockConnectionState :: MockState
+  }
+
+mockConnect :: IO (IORef MockConnection)
+mockConnect = newIORef $ MockConnection {
+  mockConnectionStack = Nothing,
+  mockConnectionState = 0
+  }
+
+instance Backend (IORef MockConnection) MockQuery where
+  backendStackExists conn = do
+    db <- readIORef conn
+    let stack = mockConnectionStack db
+    return $ isJust stack
+
+  backendCreateStack conn = do
+    db <- readIORef conn
+    let maybeStack = mockConnectionStack db
+    case maybeStack of
+      Nothing -> writeIORef conn $ db { mockConnectionStack = Just [] }
+      Just _  -> error "MockConnection: tried to create stack when one already exists!"
+
+  backendGetMigrations conn = do
+    db <- readIORef conn
+    let maybeStack = mockConnectionStack db
+    case maybeStack of
+      Nothing    -> error "MockConnection: tried to get migrations when no stack exists!"
+      Just stack -> return stack
+
+  backendDownMigrate conn (BiMigration up down _) = case down of
+    Nothing -> error "MockConnection: invalid query for down-migration"
+    Just downValid -> do       
+      db <- readIORef conn
+      let maybeStack = mockConnectionStack db
+      case maybeStack of
+        Nothing    -> error "MockConnection: tried to down-migrate when no stack exists!"
+        Just stack -> case stack of
+                        []   -> error "MockConnection: tried to down-migrate when the stack is empty!"
+                        m:ms -> if migrationUp m /= up
+                                  then error "MockConnection: tried to down-migrate a migration that isn't the top of the stack"
+                                  else writeIORef conn $ db {
+                                    mockConnectionStack = Just ms,
+                                    mockConnectionState = (mockConnectionState db) - downValid
+                                    }
+
+  backendUpMigrate conn m@(Migration up _ _) = case up of
+    Nothing -> error "MockConnection: invalid query for up-migration"
+    Just upValid -> do      
+      db <- readIORef conn
+      let maybeStack = mockConnectionStack db
+      case maybeStack of
+        Nothing    -> error "MockConnection: tried to up-migrate when no stack exists!"
+        Just stack -> writeIORef conn $ db {
+                        mockConnectionStack = Just (m:stack),
+                        mockConnectionState = (mockConnectionState db) + upValid
+                        }
---
+++ src/Database/PostgreSQL/Migrate/Test.hs
@@ -0,0 +1,15 @@
+module Database.PostgreSQL.Migrate.Test (tests) where
+
+import Distribution.TestSuite
+
+instance TestOptions String where
+  name = id
+  options = const []
+  defaultOptions _ = return (Options [])
+  check _ _ = []
+
+instance PureTestable String where
+  run s opts = Pass
+
+tests :: [Test]
+tests = [pure ("foo" :: String)]
