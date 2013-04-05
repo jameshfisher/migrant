48bc926ec6d08c7dcb1145760a60e17bc9ef8d25
James Fisher <jameshfisher@gmail.com>
2013-04-06 00:28:03 +0100

    copy over tests from Mock to MockTwo

--- migrant-base/src/Database/Migrant/Backend/MockTwo.hs
+++ migrant-base/src/Database/Migrant/Backend/MockTwo.hs
@@ -1,5 +1,6 @@
 module Database.Migrant.Backend.MockTwo where
 
+import Control.Applicative
 import Control.Monad.State
 
 import Database.Migrant.Types.Migration (Migration (..))
@@ -14,18 +15,39 @@ type MockQuery = Maybe Int -- semantics: Nothing is invalid query; (Just i) adds
 
 type MockCondition = Int -- semantics: db state has to be >= this
 
-type MockStack = Maybe [Migration MockQuery (Maybe MockQuery) MockCondition] -- head is latest
+type Mig = Migration MockQuery (Maybe MockQuery) MockCondition
+
+type MockStack = Maybe [Mig] -- head is latest
+
+data Action
+  = ActionEnsureStack
+  | ActionGetMigrations
+  | ActionBeginTransaction
+  | ActionRollbackTransaction
+  | ActionCommitTransaction
+  | ActionRunMigration MockQuery
+  | ActionPushMigration Mig
+  | ActionPopMigration
+  | ActionTestCondition MockCondition
+  deriving (Eq, Show)
 
 data MockConnection = MockConnection {
+  mockConnectionLog   :: [Action],
   mockConnectionStack :: MockStack,
   mockConnectionState :: MockState
   } deriving (Eq, Show)
 
 mockConnect :: MockConnection
 mockConnect = MockConnection {
+  mockConnectionLog = [],
   mockConnectionStack = Nothing,
   mockConnectionState = MockState Nothing 0
   }
+
+lg :: Action -> State MockConnection ()
+lg a = do
+  conn <- get
+  put $ conn { mockConnectionLog = (mockConnectionLog conn) ++ [a] }
 
 instance Backend () where
   type BackendMonad () = State MockConnection
@@ -33,6 +55,7 @@ instance Backend () where
   type BackendCond  () = MockCondition
 
   backendEnsureStack conn = do
+    lg ActionEnsureStack
     db <- get
     let maybeStack = mockConnectionStack db
     case maybeStack of
@@ -42,6 +65,7 @@ instance Backend () where
       Just _  -> return False
 
   backendGetMigrations conn = do
+    lg ActionGetMigrations
     db <- get
     let maybeStack = mockConnectionStack db
     case maybeStack of
@@ -49,6 +73,7 @@ instance Backend () where
       Just stack -> return stack
 
   backendBeginTransaction conn = do
+    lg ActionBeginTransaction
     db <- get
     let MockState rollback state = mockConnectionState db
     case rollback of
@@ -56,6 +81,7 @@ instance Backend () where
       Nothing -> put $ db { mockConnectionState = MockState (Just state) state }
 
   backendRollbackTransaction conn = do
+    lg ActionRollbackTransaction
     db <- get
     let MockState rollback _ = mockConnectionState db
     case rollback of
@@ -63,6 +89,7 @@ instance Backend () where
       Just r  -> put $ db { mockConnectionState = MockState Nothing r }
 
   backendCommitTransaction conn = do
+    lg ActionCommitTransaction
     db <- get
     let MockState rollback state = mockConnectionState db
     case rollback of
@@ -71,17 +98,20 @@ instance Backend () where
         put $ db { mockConnectionState = MockState Nothing state }
         return Nothing
 
-  backendRunMigration conn m = case m of
-    Nothing -> return $ Just "MockConnection: invalid query"
-    Just downValid -> do
-      db <- get
-      let state = mockConnectionState db
-      put $ db {
-        mockConnectionState = state { mockState = mockState state + downValid }
-        }
-      return Nothing
+  backendRunMigration conn m = do
+    lg $ ActionRunMigration m
+    case m of
+      Nothing -> return $ Just "MockConnection: invalid query"
+      Just downValid -> do
+        db <- get
+        let state = mockConnectionState db
+        put $ db {
+          mockConnectionState = state { mockState = mockState state + downValid }
+          }
+        return Nothing
 
   backendPushMigration conn m = do
+    lg $ ActionPushMigration m
     db <- get
     let maybeStack = mockConnectionStack db
     case maybeStack of
@@ -89,6 +119,7 @@ instance Backend () where
       Just stack -> put $ db { mockConnectionStack = Just (m:stack) }
 
   backendPopMigration conn = do
+    lg ActionPopMigration
     db <- get
     let maybeStack = mockConnectionStack db
     case maybeStack of
@@ -98,5 +129,6 @@ instance Backend () where
                       _:ms -> put $ db { mockConnectionStack = Just ms }
 
   backendTestCondition conn cond = do
-    MockConnection _ (MockState _ state) <- get
+    lg $ ActionTestCondition cond
+    MockState _ state <- mockConnectionState <$> get
     return $ cond <= state
